4ad16a383443501f2851a512732ffb86a9b20681
Arnaud Wissart <arnaud.wissart@live.fr>
2025-10-03 21:02:20 +0200

    refactor(api): remove legacy jwt infrastructure

--- src/inventory-api/Auth/AuthClaimTypes.cs
+++
@@ -1,8 +0,0 @@
-namespace CineBoutique.Inventory.Api.Auth;
-
-public static class AuthClaimTypes
-{
-    public const string ShopId = "shop";
-    public const string Login = "login";
-    public const string IsAdmin = "is_admin";
-}
