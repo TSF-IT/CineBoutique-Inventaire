3aa748d81fe386d3fe13dea8a22cf1de96e49f73
ArnaudW-TSF <a.wissart@tsf.fr>
2025-10-31 10:28:51 +0100

    - fix main.tsx

--- src/inventory-web/src/main.tsx
+++ src/inventory-web/src/main.tsx
@@ -1,25 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-import './index.css'
-import './styles/util-classes.css'
-import { App } from './App'
-import { initializeTheme } from './app/utils/theme'
-import { ShopProvider } from './state/ShopContext'
-import { setupPwa } from './pwa/setupPwa'
+import "./index.css";
+import "./styles/util-classes.css";
+import { App } from "./App";
+import { initializeTheme } from "./app/utils/theme";
+import { ShopProvider } from "./state/ShopContext";
+import { setupPwa } from "./pwa/setupPwa";
 
-initializeTheme()
+initializeTheme();
 
-const root = createRoot(document.getElementById('root')!)
+const root = createRoot(document.getElementById("root")!);
+
+if (import.meta.env.PROD) {
+  setupPwa();
+}
 
 root.render(
   <StrictMode>
     <ShopProvider>
       <App />
     </ShopProvider>
-  </StrictMode>,
-)
-
-if (import.meta.env.PROD) {
-  setupPwa()
-}
+  </StrictMode>
+);
